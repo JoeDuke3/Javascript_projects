a92f2f05d0f02315b9ae7ac73268a5760f23598a
JD <joeduke144@gmail.com>
2020-11-29 19:16:02 -0800

    Add comments

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,15 +1,30 @@
-window.alert("Number in your party and their names, Please.")
+// Make a window with some text pop up when the browser is opened
+window.alert
+("Number in your party and their names, Please.")
 
-var str1="The number of people in ";
-var str2=" is significantly less than ";
-var str3=" and their names are ";
-var nextName=", ";
-var lastFamilyMember=" and ";
-var endSentence=".";
-var gross=(12**2);
+// Declare 17 variables to be used in a sentence
+var 
+str1="The number of people in ", 
+Family="The Arezzinis", 
+Family=Family.fontcolor("green"), 
+str2=" is significantly less than ", 
+gross=(12**2), 
+str3=" and their names are ", 
+Dad="Jeremiah", 
+Dad=Dad.fontcolor("red"), 
+nextName=", ", 
+Mom="Hermione", 
+Mom=Mom.fontcolor("blue"), 
+Daughter="Penny", 
+Daughter=Daughter.fontcolor("cyan"), 
+lastFamilyMember=" and ", 
+Son="Zorro", 
+Son=Son.fontcolor("magenta"), 
+endSentence=".";
 
-var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
-
-var Family=Family.fontcolor("green"), Dad=Dad.fontcolor("red"), Mom=Mom.fontcolor("blue"), Daughter=Daughter.fontcolor("cyan"), Son=Son.fontcolor("magenta");
-
-document.write(str1 + Family + str2 + gross + str3+ Dad + nextName + Mom + nextName + Daughter + lastFamilyMember + Son + endSentence); 
+// Use the declared variables to construct a sentence in the document
+document.write
+(str1 + Family + str2 + gross + str3 + 
+    Dad + nextName + Mom + nextName + 
+    Daughter + lastFamilyMember + 
+    Son + endSentence);
