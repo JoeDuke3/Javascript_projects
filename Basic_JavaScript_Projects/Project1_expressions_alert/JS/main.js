da8a5c624abfc1217661cca12e9d6b7eaee6b4c0
JD <joeduke144@gmail.com>
2020-11-29 18:32:40 -0800

    First JavaScript assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Number in your party and their names, Please.")
+
+var str1="The number of people in ";
+var str2=" is significantly less than ";
+var str3=" and their names are ";
+var nextName=", ";
+var lastFamilyMember=" and ";
+var endSentence=".";
+var gross=(12**2);
+
+var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
+
+var Family=Family.fontcolor("green"), Dad=Dad.fontcolor("red"), Mom=Mom.fontcolor("blue"), Daughter=Daughter.fontcolor("cyan"), Son=Son.fontcolor("magenta");
+
+document.write(str1 + Family + str2 + gross + str3+ Dad + nextName + Mom + nextName + Daughter + lastFamilyMember + Son + endSentence); 
