a2daf09aa53e1ae1505063765324e0ebc6a0945b
JD <joeduke144@gmail.com>
2020-12-01 00:17:38 -0800

    Use comments to outline challenges

---
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,38 @@
+// Within the <body> element, create a paragraph element with the id “Math” and give it an event handler to call  		the JavaScript function you are about to create.
+function addThreeThrees() {
+    var threeCubed = 3 + 3 + 3;
+    return threeCubed;
+}
+
+//Write a subtraction operation that displays the result. Save the file.
+
+
+
+//Write a multiplication operation and a division operation and display the results of both
+
+
+
+//Write multiple mathematical operations (+, -, *, /) and display the results. Save the file.
+
+
+
+//Utilize the modulus operator (%) and display the results. Save the file.
+
+
+
+//Utilize the negation operator (-) and display the results. Save the file.
+
+
+
+//Utilize the increment operator and the decrement operator, and display the result of each.
+
+
+
+//Utilize the Math.random() function. Save the file.
+
+
+
+//utilize a Math object method and display its result.
+
+
+
