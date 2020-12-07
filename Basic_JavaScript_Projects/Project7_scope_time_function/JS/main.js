7cd5f484d8c1428825f40ad463187dfaef93c4a4
JD <joeduke144@gmail.com>
2020-12-07 02:44:34 -0800

    Format, style, overdo comments

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -1,114 +1,131 @@
+            // SUBMISSION REQUIREMENT
+            // Add comments throughout your JavaScript explaining your code
+
                     // 1ST ASSIGNMENT
                     // Write a global variable.
 
-var g = 1;
+            // SUBMISSION REQUIREMENT
+            // Assign a local variable and a global variable
+
+var g = 1;                                                  // Assign a global variable
 
                     // 1ST ASSIGNMENT
                     // Write a local variable.
 
-function fun1 () {
-    let l = 3;
-    return l;
-}
+function fun1 () {                                          // Declare and name a function
+    let l = 3;                                              // Assign a local variable
+    return l;                                               // State a return value
+}                                                           // End function
 
                     // 1ST ASSIGNMENT
                     // Intentionally write a function with an error in it 
+            // SUBMISSION REQUIREMENT
+            // Intentionally write a function with an error in it 
+            // and use the console.log() method to debug it 
+            // within the console in the Chrome Dev Tools
 
-function err_1() {
-    var x = 3;
-    return x;
-}
+function err_1() {                                          // Declare and name a function
+    var x = 3;                                              // Assign a local variable
+    return x;                                               // State a return value
+}                                                           // End a function
 
+function err_2() {                                          // Declare and name a function
+    var y = 5;                                              // Assign a local variable
+    var z = x * y;                                          // Assign a local variable
+    return z;                                               // State a return value
+}                                                           // End a function
 
-function err_2() {
-    var y = 5;
-    var z = x * y;
-    return z;
-}
+                // and use console.log() method to debug it 
+                // within the console in Chrome Dev Tools.
 
-// and use console.log() method to debug it 
-// within the console in Chrome Dev Tools.
+console.log(err_1());                                       // log a function to the console
+console.log(err_2());                                       // log a function to the console
+console.log(z);                                             // log a variable to the console
 
-console.log(err_1());
-console.log(err_2());
-console.log(z);
+var fix_x = 3;                                              // log a variable to the console
+var fix_y = 5;                                              // log a variable to the console
 
-var fix_x = 3;
-var fix_y = 5;
+function fix_function() {                                   // Declare and name a function
+    var fix_z= fix_x + fix_y;                               // Assign a local variable
+    return fix_z;                                           // State a return value
+}                                                           // End a function
 
-function fix_function() {
-    var fix_z= fix_x + fix_y;
-    return fix_z;
-}
-
-console.log(fix_function());
-console.log(fix_z);
-
+console.log(fix_function());                                // log a function to the console
+console.log(fix_z);                                         // log a variable to the console
 
                     // 2ND ASSIGNMENT
                     // Write out a function with an if statement 
                     // that utilizes the new Date().getHours() method.
 
-function alarmClock() {
-    if (new Date().getHours() > 7) {
+            // SUBMISSION REQUIREMENT
+            // Write a function that includes an if statement
+
+function alarmClock() {                                     // Declare and name a function
+    if (new Date().getHours() > 7) {                        // Begin and set arguments to an if statement
+
                     // Write out a document.getElementById() method 
                     // as covered in the previous course step 
                     // and reference the id attribute in the HTML file.
-        document.getElementById('Alert').innerHTML = "WAKE UP!!!";
-    }
-}
+
+        document.getElementById('Alert')                    // Refer to an HTML element using it's ID
+        .innerHTML = "WAKE UP!!!";                          // Assign text to an HTML element
+    }                                                       // End an if statement
+}                                                           // End a function
 
                     // 3RD ASSIGNMENT
                     // Write your own if statement.
 
-function Duh() {
-    var larger = 5;
-    var smaller =3;
-    if (larger > smaller) {
-        document.getElementById('isIt').innerHTML = "Of course it is!"
-    }
-}
+function Duh() {                                            // Declare and name a function
+    var larger = 5;                                         // Assign a local variable
+    var smaller =3;                                         // Assign a local variable
+    if (larger > smaller) {                                 // Begin and set arguments to an if statement
+        document.getElementById('isIt')                     // Refer to an HTML element using it's ID
+        .innerHTML = "Of course it is!"                     // Assign text to an HTML element
+    }                                                       // End an if statement
+}                                                           // End a function
 
                     // 4TH ASSIGNMENT
-                    // Write a function that includes an if statement and an else statement.
+                    // Write a function that includes 
+                    // an if statement and an else statement.
 
-function negotiateOffer() {
-    var offer = document.getElementById('offer').value;
-    if (offer >= 100) {
+function negotiateOffer() {                                 // Declare and name a function
+    var offer = document.getElementById('offer').value;     // Assign a local variable to a specified HTML element's value
+    if (offer >= 100) {                                     // Begin and set arguments to an if statement
+
                     // Write the relevant document.getElementById() methods 
                     // as covered in the previous course step.
-        document.getElementById('answer').innerHTML = "You're on!";
-                }
-    else {
+
+        document.getElementById('answer')                   // Refer to an HTML element using it's ID
+        .innerHTML = "You're on!";                          // Assign text to an HTML element
+                }                                           // End an if statement
+    else {                                                  // Begin an else statement
+
                     // Write the relevant document.getElementById() methods 
                     // as covered in the previous course step.
-        document.getElementById('answer').innerHTML = "Get outta here..."
-    }
-}
+
+        document.getElementById('answer')                   // Refer to an HTML element using it's ID
+        .innerHTML = "Get outta here..."                    // Assign text to an HTML element
+    }                                                       // End an else statement
+}                                                           // End a function
 
                     // 5TH ASSIGNMENT
                     // Write out all code from the previous course step 
                     // and successfully execute it in the browser. 
+            // SUBMISSION REQUIREMENT
+            // Write the Time_function() outlined in previous slides
 
-function Time_function() {
-    var Time = new Date().getHours();
-    var Reply;
-    if (Time < 12 == Time > 0) {
-        Reply = "It is morning time!";
-    }
-    else if (Time > 12 == Time < 18) {
-        Reply = "It is the afternoon";
-    }
-    else {
-        Reply = "It is evening time";
-    }
-    document.getElementById("Time_of_day").innerHTML = Reply;
-}
-
-                    // ASSIGNMENT SUBMISSION
-                    // Assign a local variable and a global variable
-                    // Write a function that includes an if statement
-                    // Intentionally write a function with an error in it 
-                    // and use the console.log() method to debug it within the console in the Chrome Dev Tools
-                    // Write the Time_function() outlined in previous slides
-                    // Add comments throughout your JavaScript explaining your code
+function Time_function() {                                  // Declare and name a function
+    var Time = new Date().getHours();                       // Get the time from the system and assign it to a local variable
+    var Reply;                                              // Name a local variable
+    if (Time < 12 == Time > 0) {                            // Begin and set arguments to an if statement
+        Reply = "It is morning time!";                      // Assign text to the value of a local variable
+    }                                                       // End an if statement
+    else if (Time > 12 == Time < 18) {                      // Begin and set arguments to an else if statement
+        Reply = "It is the afternoon";                      // Assign text to the value of a local variable
+    }                                                       // End an else if statement
+    else {                                                  // Begin an else statement
+        Reply = "It is evening time";                       // Assign text to the value of a local variable
+    }                                                       // End an else statement
+    document.getElementById("Time_of_day")                  // Refer to an HTML element using it's ID
+    .innerHTML = Reply;                                     // Assign the value of an HTML element with a local variable
+}                                                           // End a function
