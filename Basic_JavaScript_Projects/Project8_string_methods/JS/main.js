a7e1932792caec6974024c468d9d720c2fd0e260
JD <joeduke144@gmail.com>
2020-12-07 20:58:24 -0800

    Complete First Challenge

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -12,8 +12,25 @@ function startProject() {
                     // 2ND ASSIGNMENT
                     // write a slice() method 
                     // that displays a section of a string.
+var Prophecy = 
+"Our nations grew weak and our cities fell to slums while evil stood strong.";
 function inTheBeginning() {
-    var Prophecy = "Our nations grew weak and cities fell to slums while evil stood strong.";
-    var Cause = Prophecy.slice(53, 57);
+    var Cause = Prophecy.slice(57, 61);
     document.getElementById("Fear").innerHTML = Cause;
 }
+
+                    // 1ST CHALLENGE
+                    // Successfully write and execute 
+                    // the toUpperCase() method.
+                    // Successfully write and execute 
+                    // the search() method.
+function capsIt() {
+    var loud = Prophecy.toUpperCase();
+    document.getElementById("Challenge1a").innerHTML = loud;
+}
+
+function findIt() {
+    var found = Prophecy.search("strong");
+    document.getElementById("Challenge1b").innerHTML = found;
+}
+
