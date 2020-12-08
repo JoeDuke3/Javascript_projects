da5bd715f484771b0efd37c12ffc1a02569ca3d4
JD <joeduke144@gmail.com>
2020-12-08 15:39:43 -0800

    Complete CSS

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,13 +1,13 @@
-function countdown() {
-    var seconds = document.getElementById("seconds").value;
+// function countdown() {
+//     var seconds = document.getElementById("seconds").value;
 
-    function tick() {
-        seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        setTimeout(tick, 1000);
-    if(seconds == -1) {
-        alert("Time's up!");
-    }
-         }
-    tick();
-}
+//     function tick() {
+//         seconds = seconds - 1;
+//         timer.innerHTML = seconds;
+//         setTimeout(tick, 1000);
+//     if(seconds == -1) {
+//         alert("Time's up!");
+//     }
+//          }
+//     tick();
+// }
