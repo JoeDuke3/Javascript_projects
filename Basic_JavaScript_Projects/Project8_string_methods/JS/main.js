aee7e55b64e43a81c146bbc78ac6030b8a1d5772
JD <joeduke144@gmail.com>
2020-12-07 18:33:32 -0800

    Complete First Assignment

---
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,10 @@
+                    // 1ST ASIGNMENT
+                    // utilize the concat() method to 
+                    // connect two or more strings.
+function startProject() {
+    var strA = "This is ";
+    var strB = "the start of ";
+    var strC = "Project 8!";
+    var wholeSentence = strA.concat(strB, strC);
+    document.getElementById("concatenate").innerHTML = wholeSentence;
+}
