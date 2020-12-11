256364ecd0299367ccc07d4c9a27b413bb754b33
JD <joeduke144@gmail.com>
2020-12-10 16:11:51 -0800

    Comment frame

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -1,1 +1,87 @@
-alert("gitcommit");
+// 1ST ASSIGNMENTS
+// Create a while loop.
+
+
+
+// 1ST CHALLENGE
+// Use the JavaScript string length property.
+
+
+
+// 2ND ASSIGNMENT
+// Write a for loop
+
+
+
+// 3RD ASSIGNMENT
+// Write an array within a function to be called.
+
+// Utilize a function with the 
+// document.getElementById().innerHTML method 
+// to display a value (or values) of your array.
+
+
+
+// 4TH ASSIGNMENT
+// Create an object through utilization of the const keyword.
+
+// Include Properties with values.
+
+// Create 'Constant_Function; to be called from HTML that will
+// display a string using 'getId"Constant" that includes
+// a property value(s) of the object.
+
+// Change a property's value
+
+// Add a property with a value
+
+// Display a string that includes 
+// the added property value 
+// and the changed property value using 
+// document.getElementById("Constant").innerHTML method.
+
+
+
+
+// 5TH ASSIGNMENT
+// Utilize the let keyword
+
+
+
+// 2ND CHALLENGE
+// Use the JavaScript return statement.
+
+
+
+// 6TH ASSIGNMENT
+// Create an object using the let keyword. 
+// Ensure your object includes properties and a method.
+
+
+
+// 3RD CHALLENGE
+// Define and learn how to utilize the break statement.
+
+// Write a loop that includes at least one break statement.
+
+// Define and learn how to utilize the continue statement.
+
+// Write a loop that includes at least one continue statement.
+
+
+
+// ASSIGNMMENT SUBMISSION
+// Minimum project requirements:
+
+// Write a function that utilizes a while loop
+
+// Write a function that utilizes a for loop
+
+// Write a function that utilizes an array
+
+// Create an object using the “let” keyword
+
+// Add comments throughout your JavaScript explaining your code
+
+// Submit Page 205
+
