e7eb0042306677d27a739b12f931d8554883db84
JD <joeduke144@gmail.com>
2021-01-07 21:06:28 -0800

    Images for JS Course Portfolio

--- Basic_JavaScript_Projects/todo_app/js/todo.js
+++ Basic_JavaScript_Projects/todo_app/js/todo.js
@@ -1,28 +1,25 @@
 /* this function gets the task from input*/
 function get_todos() {
-    /* This creates an array of tasks that are inputed*/
+    /*This creates an array of task that are inputed*/
     var todos = new Array;
-    /* This pulls the task that was saved in the 
-    web browser memory*/
+    /*this pulls the task that was saved in the web browser memory*/
     var todos_str = localStorage.getItem('todo');
-    /* If the Input is not null then JSON.parse will 
-    communicate with the web browser to make the task
-    a JavaScript object*/
+    /*If the Input is not null then JSON.parse will 
+    communitcate with the web browser to make the task a JavaScript object*/
     if (todos_str !== null) {
         todos = JSON.parse(todos_str);
     }
     return todos;
 }
-/* This function adds the inputed task to the 
-get todos function array*/
+/*This function adds the inputed task to the get_todos function array*/
 function add() {
     /*This takes the inputed task and creates a variable of it*/
     var task = document.getElementById('task').value;
 
     var todos = get_todos();
-    /* This adds a new task at the end of the array*/
+    /*This adds a new task the end of the array*/
     todos.push(task);
-    /* This converts the task input to a JSON string */
+    /* this converts the task input to a JSON string*/
     localStorage.setItem('todo', JSON.stringify(todos));
     document.getElementById("task").value = "";
     show();
@@ -30,24 +27,44 @@ function add() {
     return false;
 }
 
-/* This function keeps the tasks permanently displayed on the screen*/
+/* this creates the functionality of removing a todo item from the array */
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    /*this looks in the show() how to display a removed item on the screen */
+    show();
+
+    return false;
+}
+
+/*this function keeps the tasks permanently displayed on the screen*/
 function show() {
-    /* This sets the task that was retrieved as a variable*/
+    /* this sets the task that was retrieved as a variable*/
     var todos = get_todos();
 
-    /* This sets up such task as an unordered list*/
+    /*This this sets up each task as an unordered list*/
     var html = '<ul>';
     /*This displays a task to the list in the order that is inputed*/
     for (var i = 0; i < todos.length; i++) {
-        /* This also displays the task as a list and creates the button with the "x"*/
-        html += '<li>' + todos[i] + '<button class"remove" id="' + i + '">x</button></li>';
-    };
+        /*this also displays the task as a list and creates the button with the "x"*/
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
 
+    };
     html += '</ul>';
-    /* This displays the task as a list*/
-    document.getElementById("todos").innerHTML = html;
+    /*This displays the task as a list*/
+    document.getElementById('todos').innerHTML = html;
+
+    /*This tells the browser how to display the todo
+    array after an item has been removed */
+    var buttons = document.getElementsByClassName('remove');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
 }
-/* This displays the inputed task when the 'Add Item" button is clicked*/
+
+/*This displays the inputed task when the 'Add Item" button is clicked*/
 document.getElementById('add').addEventListener('click', add);
-/* This will keep the inputs displayed permanently on the screen*/
+/*this will keep the inputs displayed permantaly on the screen*/
 show();
